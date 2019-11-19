af08d69a381bdbde7dc0d65e061f00264e2b1f61
Patryk Piasecki <patryk2000.1996@o2.pl>
2019-11-19 22:21:22 +0100

    Preserve user session

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,7 @@ const app = new Vue({
   store: window.app.store,
   router: window.app.router,
   template: '<App />',
+  created() {
+    this.$store.dispatch('init')
+  },
 }).$mount('#app')
--- assets/js/store.js
+++ assets/js/store.js
@@ -10,6 +10,13 @@ const store = new Vuex.Store({
   actions: {
     auth: ({ commit }, auth) => {
       commit('_auth', auth)
+
+      localStorage.setItem('app.auth', JSON.stringify(auth))
+    },
+    init({ commit }) {
+      const auth = JSON.parse(localStorage.getItem('app.auth'))
+
+      commit('_auth', auth)
     },
   },
 })
