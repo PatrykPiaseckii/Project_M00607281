00a3216fa830e3cc59d56dca6f21a8138da12533
patrykpiasecki <patryk2000.1996@o2.pl>
2020-01-29 16:10:44 +0100

    Made the server HTTPS-ready

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 const DATABASE_URL = 'mongodb://root:password@localhost:27017'
 const DATABASE_NAME = 'app'
+const CERTIFICATE_PATH = '@todo'
 
+const fs = require('fs')
+const cors = require('cors')
+const http = require('http')
+const https = require('https')
 const express = require('express')
 const { MongoClient } = require('mongodb')
 const Router = require('./modules/Router')
 const Auth = require('./modules/Auth')
+
+const privateKey = fs.readFileSync(`${CERTIFICATE_PATH}/localhost.key`, 'utf8')
+const certificate = fs.readFileSync(`${CERTIFICATE_PATH}/localhost.cert`, 'utf8')
 
 const app = express()
 const router = new Router(express.Router)
@@ -15,10 +23,15 @@ const main = async () => {
   await mongoClient.connect()
   const db = mongoClient.db(DATABASE_NAME)
 
+  app.use(cors())
   app.use(express.json())
   app.use('/api', router.setup(db, auth))
 
-  app.listen(3000, () => console.log('Started server at http://localhost:3000'))
+  const httpServer = http.createServer(app)
+  const httpsServer = https.createServer({ key: privateKey, cert: certificate }, app)
+
+  httpServer.listen(3000)
+  httpsServer.listen(8443)
 }
 
 main()
